c42e3385e153ae008eee6cbb9939976f206671cc
kaktusssssss <150985980+kaktusssssss@users.noreply.github.com>
2026-05-25 17:34:28 +0700

    cleanup

--- user_admin/includes/auth.php
+++ user_admin/includes/auth.php
@@ -7,14 +7,6 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/UserManager.php';
 
 /**
- * Check if user is logged in
- * @return bool
- */
-// function isLoggedIn() {
-//     return isset($_SESSION['user_id']);
-// }
-
-/**
  * Require authentication - redirect to login if not authenticated
  */
 function requireAuth() {
